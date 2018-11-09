cda08767b620e61e06f09cc5b5674bc7fc05fdb4
cfi <carlo@field.li>
2018-11-09 18:43:56 +0100

    add frontend
    Signed-off-by: cfi <carlo@field.li>

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { BackendService } from './backend.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [BackendService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
