30dc4cf747866915e9d481821e27f927b502ef6f
cfi <carlo@field.li>
2018-11-09 18:42:02 +0100

    update backends
    Signed-off-by: cfi <carlo@field.li>

--- backend/java-backend/src/main/java/ch/letsboot/deployaton/Main.java
+++ backend/java-backend/src/main/java/ch/letsboot/deployaton/Main.java
@@ -5,7 +5,8 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World!");
+        port(8080);
+        get("/name", (req, res) -> System.getenv("SPARK_NAME"));
     }
 
 }
