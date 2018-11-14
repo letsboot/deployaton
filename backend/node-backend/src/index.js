f0c56c0f0ed5ab2374b707bb29d31c298f832f17
cfi <carlo@field.li>
2018-11-14 11:22:07 +0100

    plain node deployment
    Signed-off-by: cfi <carlo@field.li>

--- backend/node-backend/src/index.js
+++ backend/node-backend/src/index.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.use(cors())
 
+app.get('/', (req, res) => res.send('hello'))
 app.get('/name', (req, res) => res.send('express'))
 
 app.listen(3000, () => console.log('Express server started and listening.'))
