2e59a1e8107a28956cde7ba72ae5a91c2fa3de02
cfi <carlo@field.li>
2018-11-16 10:46:31 +0100

    add sql configuration
    Signed-off-by: cfi <carlo@field.li>

--- backend-complete/node-backend/src/index.js
+++ backend-complete/node-backend/src/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const db = require('simple-postgres');
 
 app.use(cors())
 
 app.get('/', (req, res) => res.send('hello'))
 app.get('/name', (req, res) => res.send('express'))
+app.get('/users', async (req, res) => {
+    let users = await db.rows('SELECT * FROM SOME_TABLE')
+    res.send(users);
+})
 
 app.listen(3000, () => console.log('Express server started and listening.'))
